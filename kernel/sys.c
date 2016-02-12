a0219f816eff17fb79637735eee3cbbde8a8d1d9
tamirda <smstamir@gmail.com>
2016-02-12 18:07:39 +0200

    Linux 3.4.41

--- kernel/sys.c
+++ kernel/sys.c
@@ -371,7 +371,6 @@ void kernel_restart_prepare(char *cmd)
 	system_state = SYSTEM_RESTART;
 	usermodehelper_disable();
 	device_shutdown();
-	syscore_shutdown();
 }
 
 /**
@@ -420,6 +419,7 @@ void kernel_restart(char *cmd)
 #endif
 	kernel_restart_prepare(cmd);
 	disable_nonboot_cpus();
+	syscore_shutdown();
 	if (!cmd)
 		printk(KERN_EMERG "Restarting system.\n");
 	else
@@ -445,6 +445,7 @@ static void kernel_shutdown_prepare(enum system_states state)
 void kernel_halt(void)
 {
 	kernel_shutdown_prepare(SYSTEM_HALT);
+	disable_nonboot_cpus();
 	syscore_shutdown();
 	printk(KERN_EMERG "System halted.\n");
 	kmsg_dump(KMSG_DUMP_HALT);
