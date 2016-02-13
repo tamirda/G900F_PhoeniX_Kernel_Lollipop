f14a16274aeffeb101406b7a5e2ca5aece6bef87
tamirda <smstamir@gmail.com>
2016-02-13 14:46:59 +0200

    Fix compilation error

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -1443,7 +1443,6 @@ static int hub_probe(struct usb_interface *intf, const struct usb_device_id *id)
 	desc = intf->cur_altsetting;
 	hdev = interface_to_usbdev(intf);
 
-	if (!hdev->parent)
 	/*
 	 * Hubs have proper suspend/resume support, except for root hubs
 	 * where the controller driver doesn't have bus_suspend and
